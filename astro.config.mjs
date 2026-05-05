ea312d47e03ba24e03cb62fddcc472d5d0e5d930
Niya Doyle <niya.doyle64@journalism.cuny.edu>
2026-05-04 20:52:23 -0400

    Change base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://github.com/mudbit-dotcom",
-	base: "/niyas_sit_temp",
+	base: "/",
 	integrations: [sitemap()],
 	markdown: {
 		shikiConfig: {
